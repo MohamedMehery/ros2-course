010c0f4364908252cf2d5e4876c62082c7adf42b
MohamedMehery <es-mohamed.abdelnasser1415@alexu.edu.eg>
2020-10-23 20:16:32 +0200

    dev

---
+++ more_interfaces/src/publish_address_book.cpp
@@ -0,0 +1,52 @@
+#include <chrono>
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "more_interfaces/msg/address_book.hpp"
+
+using namespace std::chrono_literals;
+
+/*Create a node and an AddressBook publisher.*/
+class AddressBookPublisher : public rclcpp::Node
+{
+public:
+  AddressBookPublisher()
+  : Node("address_book_publisher")
+  {
+    address_book_publisher_ =
+      this->create_publisher<more_interfaces::msg::AddressBook>("address_book", 10);
+      
+    /*Populate AddressBook fields.*/
+    auto publish_msg = [this]() -> void {
+        auto message = more_interfaces::msg::AddressBook();
+
+        message.first_name = "John";
+        message.last_name = "Doe";
+        message.age = 30;
+        message.gender = message.MALE;
+        message.address = "unknown";
+
+      /*Finally send the message periodically.*/
+        std::cout << "Publishing Contact\nFirst:" << message.first_name <<
+          "  Last:" << message.last_name << std::endl;
+
+        this->address_book_publisher_->publish(message);
+      };
+    /*Create a 1 second timer to call our publish_msg function every second.*/
+    timer_ = this->create_wall_timer(1s, publish_msg);
+  }
+
+private:
+  rclcpp::Publisher<more_interfaces::msg::AddressBook>::SharedPtr address_book_publisher_;
+  rclcpp::TimerBase::SharedPtr timer_;
+};
+
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<AddressBookPublisher>());
+  rclcpp::shutdown();
+
+  return 0;
+}
